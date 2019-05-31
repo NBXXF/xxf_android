de3449469aba8c8347c7009757c10a3e4c5c157b
NBXXF <2767356582@qq.com>
2019-05-31 10:53:26 +0800

    fix:多状态布局提供rxjava 控制

---
+++ xxf_view/src/main/java/com/xxf/view/databinding/statelayout/AlphaStateViewTransformer.java
@@ -0,0 +1,58 @@
+package com.xxf.view.databinding.statelayout;
+
+import android.support.annotation.NonNull;
+
+
+import com.xxf.arch.XXF;
+import com.xxf.arch.rxjava.transformer.internal.UILifeTransformerImpl;
+import com.xxf.view.loading.ViewState;
+
+/**
+ * @author youxuan  E-mail:xuanyouwu@163.com
+ * @Description 多状态布局rx 变换器
+ */
+public class AlphaStateViewTransformer<T> extends UILifeTransformerImpl<T> {
+
+    private IStateLayoutVM alphaStateViewVM;
+
+    public IStateLayoutVM getAlphaStateViewVM() {
+        return alphaStateViewVM;
+    }
+
+    public AlphaStateViewTransformer(@NonNull IStateLayoutVM stateViewVM) {
+        this.alphaStateViewVM = stateViewVM;
+    }
+
+    @Override
+    public void onSubscribe() {
+        if (alphaStateViewVM != null) {
+            alphaStateViewVM.setLayoutState(ViewState.VIEW_STATE_LOADING);
+        }
+    }
+
+    @Override
+    public void onComplete() {
+        if (alphaStateViewVM != null) {
+            alphaStateViewVM.setLayoutState(ViewState.VIEW_STATE_CONTENT);
+        }
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        if (alphaStateViewVM != null) {
+            alphaStateViewVM.setLayoutState(ViewState.VIEW_STATE_ERROR);
+            try {
+                alphaStateViewVM.setErrorDesc(XXF.getErrorConvertFunction().apply(throwable));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onCancel() {
+        if (alphaStateViewVM != null) {
+            alphaStateViewVM.setLayoutState(ViewState.VIEW_STATE_CONTENT);
+        }
+    }
+}
