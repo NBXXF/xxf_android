d70b9f115928915820509c83ce458e30002256b3
NBXXF <2767356582@qq.com>
2019-05-31 19:46:45 +0800

    feat:支持binding height 和width

---
+++ xxf_view/src/main/java/com/xxf/view/databinding/view/ViewBindingAdapter.java
@@ -0,0 +1,32 @@
+package com.xxf.view.databinding.view;
+
+import android.databinding.BindingAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author youxuan  E-mail:xuanyouwu@163.com
+ * @Description 扩展view的绑定
+ */
+public class ViewBindingAdapter extends android.databinding.adapters.ViewBindingAdapter {
+
+
+    @BindingAdapter("android:layout_width")
+    public static void setLayoutWidth(View view, int width) {
+        ViewGroup.LayoutParams params = view.getLayoutParams();
+        if (params.width != width) {
+            params.width = width;
+            view.setLayoutParams(params);
+        }
+    }
+
+    @BindingAdapter("android:layout_height")
+    public static void setLayoutHeight(View view, int height) {
+        ViewGroup.LayoutParams params = view.getLayoutParams();
+        if (params.height != height) {
+            params.height = height;
+            view.setLayoutParams(params);
+        }
+    }
+
+}
