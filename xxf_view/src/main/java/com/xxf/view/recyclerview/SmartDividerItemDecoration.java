c51e2f3e1bf64ef2660413aacdeb374420d7070e
NBXXF <2767356582@qq.com>
2019-06-06 10:45:22 +0800

    feat:1.6.9 分割线加强

--- xxf_view/src/main/java/com/xxf/view/recyclerview/SmartDividerItemDecoration.java
+++ xxf_view/src/main/java/com/xxf/view/recyclerview/SmartDividerItemDecoration.java
@@ -1,11 +1,17 @@
 package com.xxf.view.recyclerview;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.CallSuper;
 import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.xxf.arch.utils.DensityUtil;
 
 /**
  * @author youxuan  E-mail:xuanyouwu@163.com
@@ -13,13 +19,25 @@ import android.support.v7.widget.DividerItemDecoration;
  */
 public class SmartDividerItemDecoration extends DividerItemDecoration {
 
+    private int px;
+    private Drawable mDivider;
+    private int mOrientation;
+
     public SmartDividerItemDecoration(Context context, int orientation) {
         super(context, orientation);
     }
 
     @Override
+    public void setOrientation(int orientation) {
+        super.setOrientation(orientation);
+        this.mOrientation = orientation;
+    }
+
+    @CallSuper
+    @Override
     public void setDrawable(@NonNull Drawable drawable) {
         super.setDrawable(drawable);
+        this.mDivider = drawable;
     }
 
     /**
@@ -28,15 +46,10 @@ public class SmartDividerItemDecoration extends DividerItemDecoration {
      * @param drawable
      * @param px
      */
-    public void setDrawable(@NonNull Drawable drawable, int px) {
-        try {
-            if (px > 0) {
-                drawable.setBounds(0, 0, px, px);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public SmartDividerItemDecoration setDrawable(@NonNull Drawable drawable, int px) {
+        this.px = px;
         this.setDrawable(drawable);
+        return this;
     }
 
     /**
@@ -45,15 +58,36 @@ public class SmartDividerItemDecoration extends DividerItemDecoration {
      * @param color
      * @param px
      */
-    public void setDrawable(@ColorInt int color, int px) {
+    public SmartDividerItemDecoration setDrawable(@ColorInt int color, int px) {
+        this.px = px;
         ColorDrawable colorDrawable = new ColorDrawable(color);
-        try {
-            if (px > 0) {
-                colorDrawable.setBounds(0, 0, px, px);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
         this.setDrawable(colorDrawable);
+        return this;
+    }
+
+    /**
+     * 设置分割线 dp
+     *
+     * @param color
+     * @param dp
+     */
+    public SmartDividerItemDecoration setDrawableDp(@ColorInt int color, int dp) {
+        this.px = DensityUtil.dip2px(dp);
+        ColorDrawable colorDrawable = new ColorDrawable(color);
+        this.setDrawable(colorDrawable);
+        return this;
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        if (mDivider == null) {
+            outRect.set(0, 0, 0, 0);
+            return;
+        }
+        if (mOrientation == VERTICAL) {
+            outRect.set(0, 0, 0, px > 0 ? px : mDivider.getIntrinsicHeight());
+        } else {
+            outRect.set(0, 0, px > 0 ? px : mDivider.getIntrinsicWidth(), 0);
+        }
     }
 }
