c0a21b379ea868c4dc35d59646113735ae765c6e
NBXXF <2767356582@qq.com>
2019-06-11 07:32:18 +0800

    feat:1.7.5 fragment工具类

--- xxf_arch/src/main/java/com/xxf/arch/utils/FragmentUtils.java
+++ xxf_arch/src/main/java/com/xxf/arch/utils/FragmentUtils.java
@@ -11,6 +11,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 
 
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -46,7 +47,8 @@ public class FragmentUtils {
      * @param tag
      */
     public static Fragment addFragment(@NonNull FragmentManager fragmentManager,
-                                       @NonNull Fragment fragment, String tag) {
+                                       @NonNull Fragment fragment,
+                                       @Nullable String tag) {
         Objects.requireNonNull(fragment, "fragment is null");
         Objects.requireNonNull(fragmentManager, "fragmentManager is null");
         FragmentTransaction transaction = fragmentManager.beginTransaction();
@@ -55,38 +57,62 @@ public class FragmentUtils {
         return fragment;
     }
 
+    /**
+     * 替换碎片
+     *
+     * @param fragmentManager
+     * @param containerViewId
+     * @param fragment
+     * @param tag
+     * @return
+     */
+    public static Fragment replaceFragment(FragmentManager fragmentManager,
+                                           @IdRes int containerViewId,
+                                           @Nullable Fragment fragment,
+                                           @Nullable String tag) {
+        Objects.requireNonNull(fragmentManager, "fragmentManager is null");
+        Objects.requireNonNull(fragment, "fragment is null");
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(containerViewId, fragment, tag);
+        transaction.commitAllowingStateLoss();
+        return fragment;
+    }
 
     /**
      * 显示隐藏碎片
      *
      * @param fragmentManager
+     * @param fragment
      * @param containerViewId
-     * @param from
-     * @param to
      */
-    public static Fragment switchFragment(FragmentManager fragmentManager, @IdRes int containerViewId, @Nullable Fragment from, @NonNull Fragment to) {
+    public static Fragment switchFragment(FragmentManager fragmentManager,
+                                          @Nullable Fragment fragment,
+                                          @IdRes int containerViewId) {
         Objects.requireNonNull(fragmentManager, "fragmentManager is null");
-        Objects.requireNonNull(to, "to fragment is null");
+        Objects.requireNonNull(fragment, "fragment is null");
+
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         // 先判断是否被add过
-        if (!to.isAdded()) {
-            if (from == null) {
-                transaction
-                        .add(containerViewId, to)
-                        .commitAllowingStateLoss();
-            } else {
-                transaction
-                        .hide(from)
-                        .add(containerViewId, to)
-                        .commitAllowingStateLoss();
-            }
-        } else {
+        if (!fragment.isAdded()) {
             transaction
-                    .hide(from)
-                    .show(to)
+                    .add(containerViewId, fragment)
                     .commitAllowingStateLoss();
+        } else {
+            List<Fragment> fragments = fragmentManager.getFragments();
+            for (Fragment f : fragments) {
+                if (f == fragment) {
+                    if (f.isHidden()) {
+                        transaction.show(f);
+                    }
+                } else {
+                    if (!f.isHidden()) {
+                        transaction.hide(f);
+                    }
+                }
+            }
+            transaction.commitAllowingStateLoss();
         }
-        return to;
+        return fragment;
     }
 
 }
