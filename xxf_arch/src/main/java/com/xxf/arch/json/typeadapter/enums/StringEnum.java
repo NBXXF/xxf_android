72d277ffddb148a82c2765dc8ec018cebf727666
NBXXF <2767356582@qq.com>
2019-06-17 14:35:54 +0800

    feat:自定义枚举转换 支持 字符串类型

---
+++ xxf_arch/src/main/java/com/xxf/arch/json/typeadapter/enums/StringEnum.java
@@ -0,0 +1,14 @@
+package com.xxf.arch.json.typeadapter.enums;
+
+/**
+ * @author youxuan  E-mail:xuanyouwu@163.com
+ * @Description
+ */
+public interface StringEnum {
+    /**
+     * 获取整数
+     *
+     * @return
+     */
+    String getValue();
+}
