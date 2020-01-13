666c5baed58d02ad32242246d629e5ebe55ea4a6
NBXXF <2767356582@qq.com>
2020-01-13 15:03:01 +0800

    fix:setTransparent导致导航栏颜色透明

--- xxf_view/src/main/java/com/xxf/view/utils/StatusBarUtils.java
+++ xxf_view/src/main/java/com/xxf/view/utils/StatusBarUtils.java
@@ -686,7 +686,8 @@ public class StatusBarUtils {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            //fix by xyw 导致导航栏也是透明色
+            // activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
             activity.getWindow().setStatusBarColor(Color.TRANSPARENT);
         } else {
             activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
