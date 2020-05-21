4d75b74d6f92ae85c92c5019f42fb61ddad12257
XGod <xuanyouwu@163.com>
2020-05-21 17:30:29 +0800

    feat:actionsheet 支持选中

--- xxf_view/src/main/java/com/xxf/view/actiondialog/ItemMenu.java
+++ xxf_view/src/main/java/com/xxf/view/actiondialog/ItemMenu.java
@@ -1,12 +1,14 @@
 package com.xxf.view.actiondialog;
 
+import com.xxf.view.recyclerview.adapter.SelectableEntity;
+
 /**
  * @author xuanyouwu
  * @email xuanyouwu@163.com
  * @time 2016-09-21 15:45
  * item 渲染控制模型
  */
-public interface ItemMenu<T> {
+public interface ItemMenu<T> extends SelectableEntity {
 
     T getItem();
 
--- xxf_view/src/main/java/com/xxf/view/actiondialog/ItemMenuImpl.java
+++ xxf_view/src/main/java/com/xxf/view/actiondialog/ItemMenuImpl.java
@@ -9,6 +9,7 @@ public class ItemMenuImpl<T> implements ItemMenu<T> {
     public int itemColor = 0xFF333333;//默认#333333
     public CharSequence itemTitle;
     public boolean itemDisable;
+    public boolean isItemSelected;
 
     public ItemMenuImpl(T item, CharSequence itemTitle) {
         this.item = item;
@@ -20,6 +21,14 @@ public class ItemMenuImpl<T> implements ItemMenu<T> {
         this.itemColor = itemColor;
         this.itemTitle = itemTitle;
         this.itemDisable = itemDisable;
+    }
+
+    public ItemMenuImpl(T item, int itemColor, CharSequence itemTitle, boolean itemDisable, boolean isItemSelected) {
+        this.item = item;
+        this.itemColor = itemColor;
+        this.itemTitle = itemTitle;
+        this.itemDisable = itemDisable;
+        this.isItemSelected = isItemSelected;
     }
 
     @Override
@@ -40,5 +49,20 @@ public class ItemMenuImpl<T> implements ItemMenu<T> {
     @Override
     public boolean isItemDisable() {
         return itemDisable;
+    }
+
+    @Override
+    public boolean isItemSelected() {
+        return this.isItemSelected;
+    }
+
+    @Override
+    public void setItemSelect(boolean select) {
+        this.isItemSelected = select;
+    }
+
+    @Override
+    public void toggleItemSelect() {
+        this.isItemSelected = !this.isItemSelected;
     }
 }
