7b706848d67f89f9ddfe1eba6b0434aa722e2a0a
XGode <xuanyouwu@163.com>
2020-06-22 15:24:14 +0800

    feat:升级okhttp

--- xxf_http/src/main/java/com/xxf/arch/http/interceptor/XXFHttpLoggingInterceptor.java
+++ xxf_http/src/main/java/com/xxf/arch/http/interceptor/XXFHttpLoggingInterceptor.java
@@ -116,7 +116,7 @@ public class XXFHttpLoggingInterceptor implements Interceptor {
         Logger DEFAULT = new Logger() {
             @Override
             public void log(String message) {
-                Platform.get().log(INFO, message, null);
+                Platform.get().log(message, INFO, null);
             }
         };
     }
