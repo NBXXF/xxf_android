c3a4d270ed276ae297c03f3648e62ed0d9cb57ea
XXF <xuanyouwu@163.com>
2020-07-24 12:04:49 +0800

    feat:XXF 支持高度自适应的viewpager

---
+++ xxf_view/src/main/java/com/xxf/view/viewpager/WrapContentHeightViewPager.java
@@ -0,0 +1,40 @@
+package com.xxf.view.viewpager;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.viewpager.widget.ViewPager;
+
+/**
+ * @author xuanyouwu
+ * @email xuanyouwu@163.com
+ * @time 2016-11-10 17:50
+ * 高度自适应
+ */
+public class WrapContentHeightViewPager extends ViewPager {
+    public WrapContentHeightViewPager(Context context) {
+        super(context);
+    }
+
+    public WrapContentHeightViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int height = 0;
+        for (int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+            child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+            int h = child.getMeasuredHeight();
+            if (h > height) {
+                height = h;
+            }
+        }
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
