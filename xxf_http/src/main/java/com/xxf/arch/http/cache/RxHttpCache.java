4d81d920cac34687d96f4389b3fe786ceb6cb232
XXF <xuanyouwu@163.com>
2020-08-01 11:47:24 +0800

    feat:XXF 支持Post缓存

--- xxf_http/src/main/java/com/xxf/arch/http/cache/RxHttpCache.java
+++ xxf_http/src/main/java/com/xxf/arch/http/cache/RxHttpCache.java
@@ -3,20 +3,26 @@ package com.xxf.arch.http.cache;
 
 
 import android.os.Build;
+
 import androidx.annotation.RequiresApi;
+
 import android.text.TextUtils;
 
 import com.xxf.arch.http.cache.disklrucache.SimpleDiskLruCache;
+import com.xxf.arch.http.interceptor.HttpLoggingInterceptor;
 import com.xxf.arch.json.JsonUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 import io.reactivex.annotations.Nullable;
 import okhttp3.HttpUrl;
 import okhttp3.MediaType;
 import okhttp3.Request;
 import okhttp3.ResponseBody;
+import okio.Buffer;
 import okio.ByteString;
 import retrofit2.Converter;
 import retrofit2.Response;
@@ -44,33 +50,67 @@ public class RxHttpCache {
         return ByteString.encodeUtf8(url.toString()).md5().hex();
     }
 
+    public static String key(String key) {
+        return ByteString.encodeUtf8(key).md5().hex();
+    }
+
     @Nullable
     public <T> Response<T> get(Request request, Converter<ResponseBody, T> responseConverter) {
         if (cache == null) {
             return null;
         }
         String requestMethod = request.method();
-        if (requestMethod.equals("GET")) {
+        if (TextUtils.equals(requestMethod, "GET")) {
             String key = key(request.url());
-            SimpleDiskLruCache.StringEntry stringEntry = null;
+            return getResponseByKey(key, responseConverter);
+        } else if (requestMethod.equals("POST")) {
             try {
-                stringEntry = cache.getString(key);
-            } catch (IOException e) {
+                Buffer buffer = new Buffer();
+                request.body().writeTo(buffer);
+
+                Charset charset = StandardCharsets.UTF_8;
+                MediaType reqContentType = request.body().contentType();
+                if (reqContentType != null) {
+                    charset = reqContentType.charset(StandardCharsets.UTF_8);
+                }
+                if (HttpLoggingInterceptor.isPlaintext(buffer)) {
+                    String key = key(request.url() + buffer.clone().readString(charset));
+                    return getResponseByKey(key, responseConverter);
+                }
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-            if (stringEntry == null) {
-                return null;
-            }
-            String jsonCache = stringEntry.getString();
-            if (TextUtils.isEmpty(jsonCache)) {
-                return null;
-            }
-            final MediaType MEDIA_TYPE = MediaType.get("application/json; charset=UTF-8");
-            try {
-                return Response.success(responseConverter.convert(ResponseBody.create(MEDIA_TYPE, jsonCache)));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        }
+        return null;
+    }
+
+    /**
+     * 获取缓存
+     *
+     * @param key
+     * @param responseConverter
+     * @param <T>
+     * @return
+     */
+    private <T> Response<T> getResponseByKey(String key, Converter<ResponseBody, T> responseConverter) {
+        SimpleDiskLruCache.StringEntry stringEntry = null;
+        try {
+            stringEntry = cache.getString(key);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (stringEntry == null) {
+            return null;
+        }
+        String jsonCache = stringEntry.getString();
+        if (TextUtils.isEmpty(jsonCache)) {
+            return null;
+        }
+        final MediaType MEDIA_TYPE = MediaType.get("application/json; charset=UTF-8");
+        try {
+            return Response.success(responseConverter.convert(ResponseBody.create(MEDIA_TYPE, jsonCache)));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return null;
     }
@@ -82,15 +122,40 @@ public class RxHttpCache {
         }
         String requestMethod = response.raw().request().method();
         String contentType = response.headers().get("Content-Type");
-        if (requestMethod.equals("GET")
-                && contentType != null
-                && contentType.toLowerCase().startsWith("application/json")) {
+        /**
+         * 只缓存json
+         */
+        boolean supportContentCache = contentType != null && contentType.toLowerCase().startsWith("application/json");
+        if (supportContentCache) {
             Request request = response.raw().request();
-            String key = key(request.url());
-            try {
-                cache.put(key, JsonUtils.toJsonString(response.body()));
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (TextUtils.equals(requestMethod, "GET")) {
+                String key = key(request.url());
+                try {
+                    cache.put(key, JsonUtils.toJsonString(response.body()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else if (requestMethod.equals("POST")) {
+                try {
+                    Buffer buffer = new Buffer();
+                    request.body().writeTo(buffer);
+
+                    Charset charset = StandardCharsets.UTF_8;
+                    MediaType reqContentType = request.body().contentType();
+                    if (reqContentType != null) {
+                        charset = reqContentType.charset(StandardCharsets.UTF_8);
+                    }
+                    if (HttpLoggingInterceptor.isPlaintext(buffer)) {
+                        String key = key(request.url() + buffer.clone().readString(charset));
+                        try {
+                            cache.put(key, JsonUtils.toJsonString(response.body()));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
