860365be9746b6430df3adef94fd74b6d30b7373
XXF <xuanyouwu@163.com>
2020-12-29 15:06:57 +0800

    feat:升级okhttp

--- xxf_http/src/main/java/com/xxf/arch/http/ResponseException.java
+++ xxf_http/src/main/java/com/xxf/arch/http/ResponseException.java
@@ -7,13 +7,13 @@ import java.io.IOException;
 /**
  * Description
  * <p>
+ *
  * @Author: XGod  xuanyouwu@163.com  17611639080  https://github.com/NBXXF     https://blog.csdn.net/axuanqq
  * date createTime：2017/4/5
  * version 1.0.0
  */
 public class ResponseException extends IOException {
     public final int code;
-    public final String message;
     @Nullable
     public final Object body;
 
@@ -25,15 +25,13 @@ public class ResponseException extends IOException {
     public ResponseException(int code, String message, Object body) {
         super(message);
         this.code = code;
-        this.message = message;
         this.body = body;
     }
 
     @Override
     public String toString() {
         return "ResponseException{" +
-                "code=" + code +
-                ", message='" + message + '\'' +
+                "code=" + code + super.getMessage() +
                 ", body=" + body +
                 '}';
     }
