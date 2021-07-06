30e7ceaa54767887de21507acfa85c929a312a09
xxf <xuanyouwu@163.com>
2021-07-06 14:09:19 +0800

    fix:ob mergefunction

--- lib_objectbox/src/main/java/com/xxf/objectbox/ObjectBoxMergeFunction.kt
+++ lib_objectbox/src/main/java/com/xxf/objectbox/ObjectBoxMergeFunction.kt
@@ -7,7 +7,7 @@ import io.reactivex.rxjava3.functions.BiFunction
  * @Author: XGod
  * @CreateDate: 2020/7/25 23:00
  */
-interface ObjectBoxMergeFunction<T> : BiFunction<List<T>?, Map<Long?, T>?, List<T>?> {
+interface ObjectBoxMergeFunction<T> : BiFunction<List<T>, Map<Long, T>, List<T>> {
     @Throws(Throwable::class)
-    override fun apply(insertList: List<T>?, localDataInDb: Map<Long?, T>?): List<T>?
+    override fun apply(insertList: List<T>, localDataInDb: Map<Long, T>): List<T>
 }
