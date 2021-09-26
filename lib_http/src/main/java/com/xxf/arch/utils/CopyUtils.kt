64a8367fcd69ec711a1ea0d4e8f1bb1f9f93e9f4
xxf <xuanyouwu@163.com>
2021-09-26 16:37:47 +0800

    copy 加强子类和父类之间的转换

--- lib_http/src/main/java/com/xxf/arch/utils/CopyUtils.kt
+++ lib_http/src/main/java/com/xxf/arch/utils/CopyUtils.kt
@@ -12,7 +12,15 @@ import com.xxf.arch.json.JsonUtils
  * 深拷贝
  */
 inline fun <reified T> T.copy(): T {
-    return CopyUtils.copy(this)
+    return CopyUtils.copy(this, T::class.java)
+}
+
+/**
+ * 深拷贝
+ * 父类和子类对象 可以双向转换复制
+ */
+inline fun <reified T, R> T.copy(toClass: Class<R>): R {
+    return CopyUtils.copy(this, toClass)
 }
 
 /**
@@ -20,14 +28,28 @@ inline fun <reified T> T.copy(): T {
  */
 object CopyUtils {
     inline fun <reified T> copy(t: T): T {
+        return copy(t, T::class.java)
+    }
+
+    fun <T, R> copy(t: T, toClass: Class<R>): R {
         val toJsonString = JsonUtils.toJsonString(t);
-        return JsonUtils.toBean(toJsonString, T::class.java)
+        return JsonUtils.toBean(toJsonString, toClass)
     }
 
     inline fun <reified T> copySafe(t: T): T? {
         try {
             val toJsonString = JsonUtils.toJsonString(t);
             return JsonUtils.toBean(toJsonString, T::class.java)
+        } catch (e: Throwable) {
+            e.printStackTrace()
+        }
+        return null
+    }
+
+    fun <T, R> copySafe(t: T, toClass: Class<R>): R? {
+        try {
+            val toJsonString = JsonUtils.toJsonString(t);
+            return JsonUtils.toBean(toJsonString, toClass)
         } catch (e: Throwable) {
             e.printStackTrace()
         }
