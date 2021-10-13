53bedcfd082a28f8e1996819e9a1723e1013f646
xxf <xuanyouwu@163.com>
2021-10-13 16:26:19 +0800

    feat:解决输入法联想词闪烁的问题2

--- lib_view_round/src/main/java/com/xxf/view/round/XXFRoundEditText.kt
+++ lib_view_round/src/main/java/com/xxf/view/round/XXFRoundEditText.kt
@@ -47,7 +47,7 @@ open class XXFRoundEditText : AppCompatEditText, XXFRoundWidget {
         }
         if (keepState) {
             val oldText = getText()
-            if (oldText != null && oldText is SpannableStringBuilder) {
+            if (oldText != null) {
                 //避免输入法联想次闪烁
                 oldText.replace(0, oldText.length, text)
             } else {
