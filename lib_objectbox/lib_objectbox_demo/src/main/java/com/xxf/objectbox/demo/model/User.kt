621885199723ddd8d232071b2da48075e3f49428
xxf <xuanyouwu@163.com>
2021-10-26 15:26:44 +0800

    feat:升级lottie动画 升级gson 升级rxjava版本

--- lib_objectbox/lib_objectbox_demo/src/main/java/com/xxf/objectbox/demo/model/User.kt
+++ lib_objectbox/lib_objectbox_demo/src/main/java/com/xxf/objectbox/demo/model/User.kt
@@ -1,14 +1,9 @@
 package com.xxf.objectbox.demo.model
 
-import com.xxf.objectbox.UniqueIndexMergePo
-import io.objectbox.Box
 import io.objectbox.Property
 import io.objectbox.annotation.Entity
 import io.objectbox.annotation.Id
 import io.objectbox.annotation.Unique
-import io.objectbox.kotlin.equal
-import io.objectbox.kotlin.inValues
-import io.objectbox.query.QueryBuilder
 
 /**
  * @Author: XGod  xuanyouwu@163.com  17611639080  https://github.com/NBXXF     https://blog.csdn.net/axuanqq
@@ -21,19 +16,11 @@ class User(
         @Id
         var _id: Long,
         @Unique
-        var name: String, var age: Int) : UniqueIndexMergePo<User> {
+        var name: String, var age: Int) {
 
 
     override fun toString(): String {
         return "User(id=$_id, name='$name', age=$age)"
-    }
-
-    override fun setMergeId(id: Long) {
-      _id=id;
-    }
-
-    override fun getMergeUniqueIndex(): Pair<Property<User>, String> {
-        return Pair(User_.name,name);
     }
 }
 
