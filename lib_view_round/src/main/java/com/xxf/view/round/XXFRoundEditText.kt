fb1b86791fe9965fdf2801dff011d3ed3e0c8feb
xxf <xuanyouwu@163.com>
2021-11-01 23:00:07 +0800

    Revert "fix:setkeeptext 保持正确"
    This reverts commit 8ce058e2

--- lib_view_round/src/main/java/com/xxf/view/round/XXFRoundEditText.kt
+++ lib_view_round/src/main/java/com/xxf/view/round/XXFRoundEditText.kt
@@ -46,14 +46,13 @@ open class XXFRoundEditText : AppCompatEditText, XXFRoundWidget {
             }
         }
         if (keepState) {
-//            val oldText = getText()
-//            if (oldText != null) {
-//                //避免输入法联想次闪烁
-//                oldText.replace(0, oldText.length, text)
-//            } else {
-//                this.setTextKeepState(text)
-//            }
-            this.setTextKeepState(text)
+            val oldText = getText()
+            if (oldText != null) {
+                //避免输入法联想次闪烁
+                oldText.replace(0, oldText.length, text)
+            } else {
+                this.setTextKeepState(text)
+            }
         } else {
             this.setText(text)
         }
