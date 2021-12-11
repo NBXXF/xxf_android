1b676e894803af9173e6283d0bd2226140ab1c74
xxf <xuanyouwu@163.com>
2021-12-11 19:49:08 +0800

    feat:去除databinding

--- lib_view/src/main/java/com/xxf/view/view/ViewBinding.kt
+++ lib_view/src/main/java/com/xxf/view/view/ViewBinding.kt
@@ -1,4 +1,4 @@
-package com.next.space.base
+package com.xxf.view.view
 
 import android.app.Dialog
 import android.view.LayoutInflater
@@ -7,6 +7,7 @@ import androidx.activity.ComponentActivity
 import androidx.databinding.ViewDataBinding
 import androidx.viewbinding.ViewBinding
 
+@Deprecated("有局限性,请使用by viewBinding", replaceWith = ReplaceWith("viewBinding"))
 fun <VB : ViewBinding> ComponentActivity.binding(inflate: (LayoutInflater) -> VB) = lazy {
     inflate(layoutInflater).also {
         setContentView(it.root)
@@ -14,10 +15,12 @@ fun <VB : ViewBinding> ComponentActivity.binding(inflate: (LayoutInflater) -> VB
     }
 }
 
+@Deprecated("有局限性,请使用by viewBinding", replaceWith = ReplaceWith("viewBinding"))
 fun <VB : ViewBinding> Dialog.binding(inflate: (LayoutInflater) -> VB) = lazy {
     inflate(layoutInflater).also { setContentView(it.root) }
 }
 
+@Deprecated("有局限性,请使用by viewBinding", replaceWith = ReplaceWith("viewBinding"))
 fun <VB : ViewBinding> ViewGroup.binding(
     inflate: (LayoutInflater, ViewGroup?, Boolean) -> VB,
     attachToParent: Boolean = true
