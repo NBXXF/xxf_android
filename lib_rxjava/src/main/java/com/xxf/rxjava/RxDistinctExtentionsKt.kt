c9fd03c6173990c4837993f627767020693fad05
xxf <xuanyouwu@163.com>
2022-01-22 14:53:07 +0800

    feat:拓展截流 去重 增加kotlin扩展函数

--- lib_rxjava/src/main/java/com/xxf/rxjava/RxDistinctExtentionsKt.kt
+++ lib_rxjava/src/main/java/com/xxf/rxjava/RxDistinctExtentionsKt.kt
@@ -12,6 +12,10 @@ import io.reactivex.rxjava3.core.*
  * 流 有变化 才执行 相近两次去重
  * distinctUntilChanged 默认是equals;这里拓展为hashcode,便于内存数据修改未刷新的问题,更加精确判断内容变化
  * 业务模型需要复写hashCode 方法
+ * 典型场景
+ * 1.高刷
+ * 2.动画避免重复执行
+ * 3.结合concat 等组合操作符 firstCache模式 提升页面速度 又避免闪动 重复刷新等
  */
 inline fun <reified T> Observable<T>.distinctUntilChangedByHash(): Observable<T> {
     return this.distinctUntilChanged { t1, t2 -> t1.hashCode() == t2.hashCode() }
@@ -21,6 +25,10 @@ inline fun <reified T> Observable<T>.distinctUntilChangedByHash(): Observable<T>
  *  流 有变化 才执行 相近两次去重
  * distinctUntilChanged 默认是equals; 这里拓展为hashcode,便于内存数据修改未刷新的问题,更加精确判断内容变化
  * 业务模型需要复写hashCode 方法
+ * 典型场景
+ * 1.高刷
+ * 2.动画避免重复执行
+ * 3.结合concat 等组合操作符 firstCache模式 提升页面速度 又避免闪动 重复刷新等
  */
 inline fun <reified T> Flowable<T>.distinctUntilChangedByHash(): Flowable<T> {
     return this.distinctUntilChanged { t1, t2 -> t1.hashCode() == t2.hashCode() }
