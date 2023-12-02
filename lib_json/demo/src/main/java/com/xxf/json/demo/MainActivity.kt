534427fa8a7d6a1153855ddc424f8631307dcb0f
XXF <xuanyouwu@163.com>
2023-12-02 17:12:24 +0800

    feat:验证moshi gson gsonbooster的性能问题

--- lib_json/demo/src/main/java/com/xxf/json/demo/MainActivity.kt
+++ lib_json/demo/src/main/java/com/xxf/json/demo/MainActivity.kt
@@ -12,8 +12,6 @@ import androidx.annotation.RequiresApi
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.annotations.Expose
-import com.spirytusz.booster.kapt.BoosterTypeAdapterFactory
-//import com.spirytusz.booster.kapt.BoosterTypeAdapterFactory
 import com.squareup.moshi.Moshi
 import com.xxf.json.Json
 import com.xxf.json.copy
@@ -60,9 +58,8 @@ companion object {
             val boost = GsonBuilder()
                 .registerTypeAdapterFactory(BoosterTypeAdapterFactory())
                 .create()
-
-            val commonTimeCost = traceOnceJson(common, json)
             val boostTimeCost = traceOnceJson(boost, json)
+            val commonTimeCost = traceOnceJson(common, json)
             println("=========================>speed:"+""""
                 common time cost: ${TimeUnit.NANOSECONDS.toMicros(commonTimeCost) / 1000.0}
                 boost time cost:  ${TimeUnit.NANOSECONDS.toMicros(boostTimeCost) / 1000.0}
@@ -78,7 +75,7 @@ companion object {
     private fun traceOnceJson(gson: Gson, json: String): Long {
         val start = SystemClock.elapsedRealtimeNanos()
         val bean = kotlin.runCatching {
-            repeat(10){
+            repeat(1){
                 gson.fromJson<Foo>(json, Foo::class.java)
             }
         }.onFailure {
@@ -92,30 +89,34 @@ companion object {
     @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     fun testGsonOrMoshi(){
         val json = json()
+
+
+
+        val take2= traceOnceJson(json){
+            repeat(1){t->
+                gson.fromJson(it,Foo::class.java)
+            }
+            gson.fromJson(it,Foo::class.java)
+        }
         val take1= traceOnceJson(json){
-            repeat(100){t->
+            repeat(1){t->
                 common.fromJson(it,Foo::class.java)
             }
             common.fromJson(it,Foo::class.java)
         }
 
         val take3= traceOnceJson(json){
-            repeat(100){t->
-                moshi.adapter<Foo>(Foo::class.java).fromJson(it)!!
+            repeat(1){t->
+                 moshi.adapter<Foo>(Foo::class.java).fromJson(it)!!
             }
             moshi.adapter<Foo>(Foo::class.java).fromJson(it)!!
         }
 
 
-        val take2= traceOnceJson(json){
-            repeat(100){t->
-                gson.fromJson(it,Foo::class.java)
-            }
-           gson.fromJson(it,Foo::class.java)
-        }
-        println("===================>ttt1:${take1/1000}")
-        println("===================>ttt2:${take2/1000}")
-        println("===================>ttt3:${take3/1000}")
+
+        println("===================>8ttt1:${take1/1000}")
+        println("===================>8ttt2:${take2/1000}")
+       // println("===================>8ttt3:${take3/1000}")
     }
 
     @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
@@ -124,6 +125,7 @@ companion object {
         val bean = kotlin.runCatching {
             callback(json)
         }.onFailure {
+            println("===================>8ttt error:${it}")
         }.getOrNull()
         val end = SystemClock.elapsedRealtimeNanos()
         return end - start
