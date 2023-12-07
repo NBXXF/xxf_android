c4b56bd8bc7f9218bdfc4ac078ef1e20c4533337
XXF <xuanyouwu@163.com>
2023-12-07 22:34:40 +0800

    feat:阀门工具增加注释

--- lib_utils/src/main/java/com/xxf/utils/RAUtils.kt
+++ lib_utils/src/main/java/com/xxf/utils/RAUtils.kt
@@ -1,48 +1,50 @@
 package com.xxf.utils
 
-import android.view.MotionEvent
-import android.view.View
-import java.util.concurrent.ConcurrentHashMap
+import android.os.SystemClock
+import java.util.WeakHashMap
 
 /**
  * @author xuanyouwu
  * @email xuanyouwu@163.com
  * @time 2016-11-10 17:50
- * 防爆点击
+ * 防爆点击，截流工具
  */
 object RAUtils {
     /**
-     * 记录一个路由上次跳转的时间
+     * 记录上次生效的时间
      */
-    private val routerJumpRecordLastTimes: MutableMap<String, Long> = ConcurrentHashMap()
+    private val effectedMap: WeakHashMap<String, Long> = WeakHashMap()
     const val DURATION_DEFAULT: Long = 500
+
+    /**
+     * 阀门是否有效
+     * @return 是否有效;true表示应该是合法的,可以通过
+     */
     val isLegalDefault: Boolean
         get() = isLegal(DURATION_DEFAULT)
 
     /**
-     * 防爆 阻力 false 表示暴力点击
-     *
-     * @param duration 点击间隔
-     * @return
+     * 阀门是否有效
+     * @param duration 阀门区间 单位 毫秒
+     * @return 是否有效;true表示应该是合法的,可以通过
      */
     fun isLegal(duration: Long): Boolean {
         return isLegal(RAUtils::class.java.name, duration)
     }
 
-    fun isLegal(path: String, duration: Long): Boolean {
-        val lastRecord = routerJumpRecordLastTimes[path]
-        val current = System.currentTimeMillis()
-        routerJumpRecordLastTimes[path] = current
-        return lastRecord == null || current - lastRecord >= duration
-    }
-
-    fun inRangeOfView(view: View, ev: MotionEvent): Boolean {
-        val location = IntArray(2)
-        view.getLocationOnScreen(location)
-        val x = location[0]
-        val y = location[1]
-        return if (ev.x < x || ev.x > x + view.width || ev.y < y || ev.y > y + view.height) {
-            false
-        } else true
+    /**
+     * 阀门是否有效
+     * @param key 唯一标识阀门钥匙
+     * @param duration 阀门区间 单位 毫秒
+     * @return 是否有效;true表示应该是合法的,可以通过
+     */
+    fun isLegal(key: String, duration: Long): Boolean {
+        val lastEffected = effectedMap.getOrPut(key) { 0L }
+        val current = SystemClock.elapsedRealtime()
+        if (current - lastEffected >= duration) {
+            effectedMap[key] = current
+            return true
+        }
+        return false
     }
 }
