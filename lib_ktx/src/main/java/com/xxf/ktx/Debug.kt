3fe2c78acbd549140dda80e6aafd6e38692eea2f
XXF <xuanyouwu@163.com>
2023-12-08 17:29:18 +0800

    feat:新增统计时长方法和只在debug执行的方法

--- lib_ktx/src/main/java/com/xxf/ktx/Debug.kt
+++ lib_ktx/src/main/java/com/xxf/ktx/Debug.kt
@@ -1,13 +1,28 @@
 package com.xxf.ktx
 
-import com.xxf.ktx.BuildConfig.DEBUG
+import android.os.SystemClock
 
 /**
  * 仅仅debug模式下才会执行
+ * @param block
+ * @return 是否是debug模式,也就是是否执行了
  */
-@Deprecated(message = "暂未实现完全")
-inline fun runDebugging(block: () -> Unit) {
-    if (DEBUG) {
+inline fun runDebugging(block: () -> Unit): Boolean {
+    if (application.isAppDebug) {
         block()
+        return true
     }
+    return false
+}
+
+/**
+ * 计算耗时
+ * @param block
+ * @return 耗时 单位纳秒
+ */
+inline fun runCosting(block: () -> Unit): Long {
+    val start = SystemClock.elapsedRealtimeNanos()
+    block()
+    val end = SystemClock.elapsedRealtimeNanos()
+    return end - start
 }
