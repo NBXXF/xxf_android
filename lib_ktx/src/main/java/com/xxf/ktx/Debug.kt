90aa525beed924ebbc7913036a0a4aade5c76a19
XXF <xuanyouwu@163.com>
2024-01-11 13:20:08 +0800

    fix:纠正方法名

--- lib_ktx/src/main/java/com/xxf/ktx/Debug.kt
+++ lib_ktx/src/main/java/com/xxf/ktx/Debug.kt
@@ -2,6 +2,7 @@ package com.xxf.ktx
 
 import android.os.SystemClock
 import kotlin.system.measureNanoTime
+import kotlin.system.measureTimeMillis
 import kotlin.time.measureTime
 import kotlin.time.measureTimedValue
 
@@ -25,9 +26,34 @@ inline fun runDebugging(block: () -> Unit): Boolean {
  * @param block
  * @return 耗时 单位纳秒
  */
+@Deprecated("纠正命名,请用runMeasureNanoTime", replaceWith = ReplaceWith("请用runMeasureNanoTime"))
 inline fun runCosting(block: () -> Unit): Long {
+    return runMeasureNanoTime(block)
+}
+
+/**
+ * 计算耗时
+ * 等价于kt自带 measureTime,measureNanoTime
+ * 只不过刨除了cpu睡眠时间
+ * @param block
+ * @return 耗时 单位纳秒
+ */
+inline fun runMeasureNanoTime(block: () -> Unit): Long {
     val start = SystemClock.elapsedRealtimeNanos()
     block()
     val end = SystemClock.elapsedRealtimeNanos()
     return end - start
+}
+
+/**
+ * 计算耗时
+ * 等价于kt自带 measureTime,measureNanoTime
+ * 只不过刨除了cpu睡眠时间
+ * @param block
+ * @return 耗时 单位纳秒
+ */
+inline fun runMeasureTimeMillis(block: () -> Unit): Long {
+    val start = SystemClock.elapsedRealtime()
+    block()
+    return SystemClock.elapsedRealtime() - start
 }
