61ace641dfa0784d49475c1e62c43b5207236165
XXF <xuanyouwu@163.com>
2024-01-24 17:37:02 +0800

    fix:依赖版本冲突

--- lib_arch/src/main/java/com/xxf/arch/fragment/navigation/NavController.kt
+++ lib_arch/src/main/java/com/xxf/arch/fragment/navigation/NavController.kt
@@ -4,11 +4,13 @@ import android.app.Activity
 import android.content.ComponentName
 import android.os.Bundle
 import android.view.View
+import androidx.annotation.IdRes
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentContainerView
+import androidx.lifecycle.LifecycleOwner
 import androidx.navigation.ActivityNavigator
-import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
 import androidx.navigation.NavHost
@@ -18,9 +20,58 @@ import androidx.navigation.findNavController
 import androidx.navigation.fragment.DialogFragmentNavigator
 import androidx.navigation.fragment.FragmentNavigator
 import androidx.navigation.fragment.NavHostFragment
+import com.xxf.application.lifecycle.findViewLifecycleOwner
 import com.xxf.arch.R
 import com.xxf.cache.ReflectionCache
+import com.xxf.ktx.asFragmentActivityOrNull
+import com.xxf.ktx.asFragmentOrNull
 
+fun FragmentContainerView.getNavHostFragment(): NavHostFragment? {
+    return when (val findViewLifecycleOwner = this.findViewLifecycleOwner()) {
+        is Fragment -> {
+            findViewLifecycleOwner?.childFragmentManager?.findFragmentById(this.id) as? NavHostFragment
+        }
+
+        is FragmentActivity -> {
+            findViewLifecycleOwner?.supportFragmentManager?.findFragmentById(this.id) as? NavHostFragment
+        }
+
+        else -> {
+            null
+        }
+    }
+}
+
+fun FragmentContainerView.getNavController(): NavController? {
+    return getNavHostFragment()?.navController
+}
+
+
+/**
+ * 通过navControllerViewId 获取上面加载的NavController
+ */
+fun LifecycleOwner.getNavHostFragment(@IdRes navControllerViewId: Int): NavHostFragment? {
+    return when (this) {
+        is Fragment -> {
+            asFragmentOrNull()?.childFragmentManager?.findFragmentById(navControllerViewId) as? NavHostFragment
+        }
+
+        is FragmentActivity -> {
+            asFragmentActivityOrNull()?.supportFragmentManager?.findFragmentById(navControllerViewId) as? NavHostFragment
+        }
+
+        else -> {
+            null
+        }
+    }
+}
+
+/**
+ * 通过navControllerViewId 获取上面加载的NavController
+ */
+fun LifecycleOwner.getNavController(@IdRes navControllerViewId: Int): NavController? {
+    return getNavHostFragment(navControllerViewId)?.navController
+}
 
 /**
  * 是否在导航控制器中
@@ -56,6 +107,64 @@ fun View.findNavControllerOrNull(): NavController? {
         null
     }
 }
+
+/**
+ * 找到容器对应的NavHost
+ */
+fun View.findNavHost(): NavHost? {
+    return this.findNavControllerView()?.getFragment<Fragment>() as? NavHost
+}
+
+/**
+ * 找到容器对应的NavHostFragment
+ */
+fun View.findNavHostFragment(): NavHostFragment? {
+    return this.findNavControllerView()?.getFragment<Fragment>() as? NavHostFragment
+}
+
+
+/**
+ * 找到 容器view
+ */
+fun View.findNavControllerView(): FragmentContainerView? {
+    return generateSequence(this) {
+        it.parent as? View?
+    }.mapNotNull {
+        //官方打的这个Tag
+        val tag = it.getTag(R.id.nav_controller_view_tag)
+        if (tag == null) null else it
+    }.firstOrNull() as? FragmentContainerView
+}
+
+/**
+ * 结束
+ * 对于DialogFragment 是dismiss
+ * 对于Activity 是 finish
+ */
+fun View.finishNav(): Boolean {
+    val findNavHostFragment = findNavHostFragment()
+    return when (val lifecycle = findNavHostFragment?.parentFragment) {
+        is DialogFragment -> {
+            lifecycle.dismissAllowingStateLoss()
+            true
+        }
+
+        is Fragment -> {
+            lifecycle.requireActivity().finish()
+            true
+        }
+
+        else -> {
+            if (findNavHostFragment != null) {
+                findNavHostFragment.requireActivity().finish()
+                true
+            } else {
+                false
+            }
+        }
+    }
+}
+
 
 /**
  * 获取回退栈数量
@@ -142,59 +251,3 @@ fun <T : DialogFragment> NavController.navigate(
     )
 }
 
-/**
- * 找到容器对应的NavHost
- */
-fun View.findNavHost(): NavHost? {
-    return this.findNavControllerView()?.getFragment<Fragment>() as? NavHost
-}
-
-/**
- * 找到容器对应的NavHostFragment
- */
-fun View.findNavHostFragment(): NavHostFragment? {
-    return this.findNavControllerView()?.getFragment<Fragment>() as? NavHostFragment
-}
-
-
-/**
- * 找到 容器view
- */
-fun View.findNavControllerView(): FragmentContainerView? {
-    return generateSequence(this) {
-        it.parent as? View?
-    }.mapNotNull {
-        //官方打的这个Tag
-        val tag = it.getTag(R.id.nav_controller_view_tag)
-        if (tag == null) null else it
-    }.firstOrNull() as? FragmentContainerView
-}
-
-/**
- * 结束
- * 对于DialogFragment 是dismiss
- * 对于Activity 是 finish
- */
-fun View.finishNav(): Boolean {
-    val findNavHostFragment = findNavHostFragment()
-    return when (val lifecycle = findNavHostFragment?.parentFragment) {
-        is DialogFragment -> {
-            lifecycle.dismissAllowingStateLoss()
-            true
-        }
-
-        is Fragment -> {
-            lifecycle.requireActivity().finish()
-            true
-        }
-
-        else -> {
-            if (findNavHostFragment != null) {
-                findNavHostFragment.requireActivity().finish()
-                true
-            } else {
-                false
-            }
-        }
-    }
-}
