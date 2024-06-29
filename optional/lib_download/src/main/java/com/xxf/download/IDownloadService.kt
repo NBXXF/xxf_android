d052d81ed754cef2cdd91573c22772d7b5b8cfac
XXF <xuanyouwu@163.com>
2024-06-29 21:32:20 +0800

    feat:升级kpower

--- optional/lib_download/src/main/java/com/xxf/download/IDownloadService.kt
+++ optional/lib_download/src/main/java/com/xxf/download/IDownloadService.kt
@@ -1,12 +1,13 @@
 package com.xxf.download
 
 import com.liulishuo.okdownload.DownloadListener
+import com.nbxxf.kpower.database.model.BasePageInfoDTO
 
 interface ICacheTaskDownloadService<T : IDownloadModel> {
     /**
      * 获取已经入库的任务
      */
-    fun getTasks(offset: Long, limit: Long, desc: Boolean): List<T>
+    fun getTasks(pageNum: Long, pageSize: Long, desc: Boolean): BasePageInfoDTO<T>
 
     /**
      * 内部执行保存
--- optional/lib_download_ob/src/main/java/com/xxf/download/ob/ObDownloadService.kt
+++ optional/lib_download_ob/src/main/java/com/xxf/download/ob/ObDownloadService.kt
@@ -1,5 +1,7 @@
 package com.xxf.download.ob
 
+import com.nbxxf.kpower.database.model.BasePageInfoDTO
+import com.nbxxf.kpower.database.objectbox.entity.BaseEntity
 import com.nbxxf.kpower.database.objectbox.service.BaseServiceImpl
 import com.xxf.download.DownloadService
 import java.io.File
@@ -14,6 +16,15 @@ abstract class ObDownloadService<E : ObDownloadModel> :
 
     protected abstract fun getService(): BaseServiceImpl<E, *>
 
+    override fun getTasks(pageNum: Long, pageSize: Long, desc: Boolean): BasePageInfoDTO<E> {
+        return getService().selectPage(pageNum, pageSize) {
+            if (desc) {
+                it.orderDesc(BaseEntity::createDate)
+            } else {
+                it.order(BaseEntity::createDate, 0)
+            }
+        }
+    }
 
     override fun onDeleteTask(tasks: List<E>) {
         getService().deleteById(tasks.map { it.id })
@@ -28,4 +39,6 @@ abstract class ObDownloadService<E : ObDownloadModel> :
         }
         getService().insertOrUpdate(tasks)
     }
+
+
 }
