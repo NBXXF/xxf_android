f5c239be9e718074a6a602648463623e5c582264
NBXXF <xuanyouwu@163.com>
2024-07-01 18:23:49 +0800

    feat:refactor download

--- optional/lib_download/src/main/java/com/xxf/download/DownloadTask.kt
+++ optional/lib_download/src/main/java/com/xxf/download/DownloadTask.kt
@@ -5,7 +5,7 @@ import com.liulishuo.okdownload.DownloadTask
 val DownloadTask.tagModelId: Int get() = 1024
 var DownloadTask.taskModel: Any?
     get() {
-        return getTag(tagModelId) as? DownloadTask
+        return getTag(tagModelId)
     }
     set(value) {
         this.addTag(tagModelId, value)
