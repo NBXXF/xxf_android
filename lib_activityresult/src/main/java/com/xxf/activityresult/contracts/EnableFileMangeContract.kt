87df0f8bcfb59c4807cabc5179775fa90ccb4e97
NBXXF <xuanyouwu@163.com>
2024-07-04 19:30:01 +0800

    feat:适配文件权限

---
+++ lib_activityresult/src/main/java/com/xxf/activityresult/contracts/EnableFileMangeContract.kt
@@ -0,0 +1,45 @@
+package com.xxf.activityresult.contracts
+
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Environment
+import android.provider.Settings
+import androidx.annotation.RequiresPermission
+import com.xxf.activityresult.contracts.setting.SettingEnableContract
+
+
+/**
+ * @Author: XGod  xuanyouwu@163.com  17611639080  https://github.com/NBXXF     https://blog.csdn.net/axuanqq  xuanyouwu@163.com  17611639080  https://github.com/NBXXF     https://blog.csdn.net/axuanqq
+ * @version 2.3.1
+ * @Description  打开文件管理  其他更多参考 [androidx.activity.result.contract.ActivityResultContracts]里面的静态类
+ *               需声明权限       <uses-permission android:name="android.permission.MANAGE_EXTERNAL_STORAGE"/>
+ * @date createTime：2020/9/5
+ */
+open class EnableFileMangeContract : SettingEnableContract() {
+    override fun createIntent(context: Context, input: Unit): Intent {
+        super.createIntent(context, input)
+        return Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION)
+    }
+
+    override fun isSupported(context: Context?): Boolean {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.R
+    }
+
+    @RequiresPermission(Manifest.permission.BLUETOOTH)
+    override fun isEnabled(context: Context?): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            Environment.isExternalStorageManager()
+        } else {
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                context?.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+            } else {
+                true
+            }
+        }
+    }
+}
--- lib_permission/src/main/java/com/xxf/permission/impl/FilePermission.kt
+++ lib_permission/src/main/java/com/xxf/permission/impl/FilePermission.kt
@@ -4,6 +4,8 @@ import android.Manifest
 import android.os.Build
 import androidx.annotation.RequiresPermission
 import androidx.lifecycle.LifecycleOwner
+import com.xxf.activityresult.contracts.EnableFileMangeContract
+import com.xxf.activityresult.startActivityForResult
 import com.xxf.permission.requestPermissionForResult
 import io.reactivex.rxjava3.core.Observable
 
@@ -12,8 +14,11 @@ import io.reactivex.rxjava3.core.Observable
  */
 @RequiresPermission(allOf = [Manifest.permission.MANAGE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE])
 inline fun <reified T : LifecycleOwner> T.requestFileReadPermissionForResult(): Observable<Map<String, Boolean>> {
-    return if (Build.VERSION.SDK_INT >= 30) {
-        requestPermissionForResult(listOf(android.Manifest.permission.MANAGE_EXTERNAL_STORAGE))
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        startActivityForResult(EnableFileMangeContract())
+            .map {
+                mapOf(Manifest.permission.MANAGE_EXTERNAL_STORAGE to it)
+            }
     } else {
         requestPermissionForResult(listOf(android.Manifest.permission.READ_EXTERNAL_STORAGE))
     }
@@ -25,7 +30,10 @@ inline fun <reified T : LifecycleOwner> T.requestFileReadPermissionForResult(): 
 @RequiresPermission(allOf = [Manifest.permission.MANAGE_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE])
 inline fun <reified T : LifecycleOwner> T.requestFileWritePermissionForResult(): Observable<Map<String, Boolean>> {
     return if (Build.VERSION.SDK_INT >= 30) {
-        requestPermissionForResult(listOf(android.Manifest.permission.MANAGE_EXTERNAL_STORAGE))
+        startActivityForResult(EnableFileMangeContract())
+            .map {
+                mapOf(Manifest.permission.MANAGE_EXTERNAL_STORAGE to it)
+            }
     } else {
         requestPermissionForResult(listOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE))
     }
