cad855882bf7f68e3d29bd5ca7d347e2c1a5cece
NBXXF <xuanyouwu@163.com>
2024-07-05 08:54:44 +0800

    feat:适配文件权限

--- lib_activityresult/src/main/java/com/xxf/activityresult/contracts/EnableFileMangeContract.kt
+++ lib_activityresult/src/main/java/com/xxf/activityresult/contracts/EnableFileMangeContract.kt
@@ -8,6 +8,7 @@ import android.os.Build
 import android.os.Environment
 import android.provider.Settings
 import androidx.annotation.RequiresPermission
+import androidx.core.content.ContextCompat
 import com.xxf.activityresult.contracts.setting.SettingEnableContract
 
 
@@ -15,7 +16,10 @@ import com.xxf.activityresult.contracts.setting.SettingEnableContract
  * @Author: XGod  xuanyouwu@163.com  17611639080  https://github.com/NBXXF     https://blog.csdn.net/axuanqq  xuanyouwu@163.com  17611639080  https://github.com/NBXXF     https://blog.csdn.net/axuanqq
  * @version 2.3.1
  * @Description  打开文件管理  其他更多参考 [androidx.activity.result.contract.ActivityResultContracts]里面的静态类
- *               需声明权限       <uses-permission android:name="android.permission.MANAGE_EXTERNAL_STORAGE"/>
+ *               需声明权限
+ *               <uses-permission android:name="android.permission.MANAGE_EXTERNAL_STORAGE"/>
+ *               <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE" />
+ *               <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
  * @date createTime：2020/9/5
  */
 open class EnableFileMangeContract : SettingEnableContract() {
@@ -28,16 +32,19 @@ open class EnableFileMangeContract : SettingEnableContract() {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.R
     }
 
-    @RequiresPermission(Manifest.permission.MANAGE_EXTERNAL_STORAGE)
+
+    @RequiresPermission(
+        allOf = [Manifest.permission.MANAGE_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE]
+    )
     override fun isEnabled(context: Context?): Boolean {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             Environment.isExternalStorageManager()
         } else {
-            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                context?.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
-            } else {
-                true
-            }
+            context?.let {
+                ContextCompat.checkSelfPermission(
+                    it, Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+            } == PackageManager.PERMISSION_GRANTED
         }
     }
 }
