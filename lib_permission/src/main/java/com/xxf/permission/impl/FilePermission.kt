e73def056426f5a29aac8cd7329767c6390b1b6f
NBXXF <xuanyouwu@163.com>
2024-07-05 09:24:01 +0800

    feat:文件权限不要重复请求

--- lib_permission/src/main/java/com/xxf/permission/impl/FilePermission.kt
+++ lib_permission/src/main/java/com/xxf/permission/impl/FilePermission.kt
@@ -1,24 +1,32 @@
 package com.xxf.permission.impl
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.os.Build
 import androidx.annotation.RequiresPermission
 import androidx.lifecycle.LifecycleOwner
 import com.xxf.activityresult.contracts.EnableFileMangeContract
 import com.xxf.activityresult.startActivityForResult
+import com.xxf.ktx.findActivity
 import com.xxf.permission.requestPermissionForResult
 import io.reactivex.rxjava3.core.Observable
 
 /**
  * 申请文件读取权限
  */
+@SuppressLint("MissingPermission")
 @RequiresPermission(allOf = [Manifest.permission.MANAGE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE])
 inline fun <reified T : LifecycleOwner> T.requestFileReadPermissionForResult(): Observable<Map<String, Boolean>> {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-        startActivityForResult(EnableFileMangeContract())
-            .map {
-                mapOf(Manifest.permission.MANAGE_EXTERNAL_STORAGE to it)
-            }
+    val enableFileMangeContract = EnableFileMangeContract()
+    return if (enableFileMangeContract.isSupported(findActivity())) {
+        if (enableFileMangeContract.isEnabled(this.findActivity())) {
+            return Observable.just(mapOf(Manifest.permission.MANAGE_EXTERNAL_STORAGE to true))
+        } else {
+            startActivityForResult(EnableFileMangeContract())
+                .map {
+                    mapOf(Manifest.permission.MANAGE_EXTERNAL_STORAGE to it)
+                }
+        }
     } else {
         requestPermissionForResult(listOf(android.Manifest.permission.READ_EXTERNAL_STORAGE))
     }
@@ -27,13 +35,19 @@ inline fun <reified T : LifecycleOwner> T.requestFileReadPermissionForResult(): 
 /**
  * 申请文件写入权限
  */
+@SuppressLint("MissingPermission")
 @RequiresPermission(allOf = [Manifest.permission.MANAGE_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE])
 inline fun <reified T : LifecycleOwner> T.requestFileWritePermissionForResult(): Observable<Map<String, Boolean>> {
-    return if (Build.VERSION.SDK_INT >= 30) {
-        startActivityForResult(EnableFileMangeContract())
-            .map {
-                mapOf(Manifest.permission.MANAGE_EXTERNAL_STORAGE to it)
-            }
+    val enableFileMangeContract = EnableFileMangeContract()
+    return if (enableFileMangeContract.isSupported(findActivity())) {
+        if (enableFileMangeContract.isEnabled(this.findActivity())) {
+            return Observable.just(mapOf(Manifest.permission.MANAGE_EXTERNAL_STORAGE to true))
+        } else {
+            startActivityForResult(EnableFileMangeContract())
+                .map {
+                    mapOf(Manifest.permission.MANAGE_EXTERNAL_STORAGE to it)
+                }
+        }
     } else {
         requestPermissionForResult(listOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE))
     }
