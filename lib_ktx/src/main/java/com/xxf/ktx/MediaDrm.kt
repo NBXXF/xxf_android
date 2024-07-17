5a48f9a9c5d1726dcac09885b794e5860d9e3296
NBXXF <xuanyouwu@163.com>
2024-07-17 15:21:34 +0800

    feat:api DEVICE_UNIQUE_ID

---
+++ lib_ktx/src/main/java/com/xxf/ktx/MediaDrm.kt
@@ -0,0 +1,22 @@
+package com.xxf.ktx
+
+import android.media.MediaDrm
+import java.util.UUID
+
+/**
+ * 数字版权的唯一id
+ */
+val DEVICE_UNIQUE_ID: String? by lazy {
+    UUID(-0x121074568629b532L, -0x5c37d8232ae2de13L)
+        .deviceUniqueId()
+}
+
+fun UUID.toMediaDrm(): MediaDrm {
+    return MediaDrm(this)
+}
+
+fun UUID.deviceUniqueId(): String? {
+    return kotlin.runCatching {
+        toMediaDrm().getPropertyByteArray(MediaDrm.PROPERTY_DEVICE_UNIQUE_ID).toHexString()
+    }.getOrNull()
+}
