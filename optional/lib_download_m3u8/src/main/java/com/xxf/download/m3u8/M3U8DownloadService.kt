479f85c12d8f54dcde3007238a1f853014e08823
NBXXF <xuanyouwu@163.com>
2024-07-23 13:44:45 +0800

    feat:m3u8合成mp4

--- optional/lib_download_m3u8/src/main/java/com/xxf/download/m3u8/M3U8DownloadService.kt
+++ optional/lib_download_m3u8/src/main/java/com/xxf/download/m3u8/M3U8DownloadService.kt
@@ -8,10 +8,6 @@ import com.xxf.download.DownloadService
 import com.xxf.download.IDownloadEntity
 import com.xxf.download.component.DownloadInfo
 import com.xxf.download.component.DownloadStatus
-import com.xxf.ktx.mkParentDirs
-import com.xxf.ktx.randomUUIDString32
-import com.xxf.log.logD
-
 /**
  * @Author: XGod  xuanyouwu@163.com  17611639080
  * Date: 22/7/24 12:07 PM
@@ -37,10 +33,8 @@ abstract class M3U8DownloadService<T : IDownloadEntity> : DownloadService<T>() {
                             UriUtil.resolve(baseUri, playlist.variants.first().url.toString())
                         clone.downloadUrl = segmentUri
                         addTask(listOf(clone))
-                        test(segmentUri)
                     }
                 } else if (playlist is HlsMediaPlaylist) {
-                    test(playlist.baseUri)
                     addTask(playlist.segments.map {
                         @Suppress("UNCHECKED_CAST") val clone = task.clone() as T
                         val baseUri: String = playlist.baseUri
@@ -50,17 +44,6 @@ abstract class M3U8DownloadService<T : IDownloadEntity> : DownloadService<T>() {
                     })
                 }
             }
-        }
-    }
-
-    private fun test(m3u8url: String) {
-        try {
-            val dir = application.cacheDir.resolve("temp")
-            dir.mkParentDirs()
-           val result= M3U8Utils.convertMp4(m3u8url, dir.resolve("$randomUUIDString32.mp4").absolutePath)
-            logD { "============>result:$result" }
-        } catch (e: Throwable) {
-            e.printStackTrace()
         }
     }
 
