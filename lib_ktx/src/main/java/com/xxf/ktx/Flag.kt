f7fe2cbf429ec4336dddecb0c01004ec1bb82775
NBXXF <xuanyouwu@163.com>
2024-07-29 16:02:14 +0800

    docs:excludeFlag

--- lib_ktx/src/main/java/com/xxf/ktx/Flag.kt
+++ lib_ktx/src/main/java/com/xxf/ktx/Flag.kt
@@ -3,6 +3,14 @@ package com.xxf.ktx
 /**
  * 是否包含某个flag
  */
-fun Int.hashFlag(flag: Int): Boolean {
+fun Int.hasFlag(flag: Int): Boolean {
     return this and flag != 0
+}
+
+/**
+ * 去除某个标记位
+ */
+fun Int.excludeFlag(valueRemoveFlag: Int): Int {
+    val resultAfterRemoval: Int = this and valueRemoveFlag.inv()
+    return resultAfterRemoval
 }
