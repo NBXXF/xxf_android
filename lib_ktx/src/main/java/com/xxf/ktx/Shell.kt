a14d35459e3477d6b6d05a1c1a5a76552f40f772
NBXXF <xuanyouwu@163.com>
2024-08-19 15:32:27 +0800

    feat:recordLogFile

--- lib_ktx/src/main/java/com/xxf/ktx/Shell.kt
+++ lib_ktx/src/main/java/com/xxf/ktx/Shell.kt
@@ -1,30 +1,43 @@
-
-
 @file:Suppress("unused")
 
 package com.xxf.ktx
 
+
+import androidx.annotation.WorkerThread
 import java.io.BufferedReader
+import java.io.File
 import java.io.InputStreamReader
 
+
+/**
+ *将日志记录到文件
+ * 请在子线程调用
+ */
+@WorkerThread
+fun recordLogFile(logFile: File) {
+    executeCmd("logcat -c")
+    executeCmd("logcat -f " + logFile.absolutePath)
+}
+
+
 fun executeCmd(command: String): String {
-  val process = Runtime.getRuntime().exec(command)
+    val process = Runtime.getRuntime().exec(command)
 
-  val resultReader = BufferedReader(InputStreamReader(process.inputStream))
-  val resultBuilder = StringBuilder()
-  var resultLine = resultReader.readLine()
-  while (resultLine != null) {
-    resultBuilder.append(resultLine)
-    resultLine = resultReader.readLine()
-  }
+    val resultReader = BufferedReader(InputStreamReader(process.inputStream))
+    val resultBuilder = StringBuilder()
+    var resultLine = resultReader.readLine()
+    while (resultLine != null) {
+        resultBuilder.append(resultLine)
+        resultLine = resultReader.readLine()
+    }
 
-  val errorReader = BufferedReader(InputStreamReader(process.errorStream))
-  val errorBuilder = StringBuilder()
-  var errorLine = errorReader.readLine()
-  while (errorLine != null) {
-    errorBuilder.append(errorLine)
-    errorLine = errorReader.readLine()
-  }
+    val errorReader = BufferedReader(InputStreamReader(process.errorStream))
+    val errorBuilder = StringBuilder()
+    var errorLine = errorReader.readLine()
+    while (errorLine != null) {
+        errorBuilder.append(errorLine)
+        errorLine = errorReader.readLine()
+    }
 
-  return "$resultBuilder\n$errorBuilder"
+    return "$resultBuilder\n$errorBuilder"
 }
