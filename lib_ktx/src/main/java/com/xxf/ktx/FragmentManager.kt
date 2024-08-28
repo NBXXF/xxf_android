066304bd58763d239483156ad7bd2d968b748eef
NBXXF <xuanyouwu@163.com>
2024-08-28 18:45:50 +0800

    fix:glide load ext kotlin

--- lib_ktx/src/main/java/com/xxf/ktx/FragmentManager.kt
+++ lib_ktx/src/main/java/com/xxf/ktx/FragmentManager.kt
@@ -6,7 +6,7 @@ import androidx.fragment.app.FragmentManager
  * @version 1.0
  * @since 2024/8/28 18:23
  */
-fun FragmentManager.clear() {
+fun FragmentManager.clearFragments() {
     beginTransaction().apply {
         fragments.forEach {
             remove(it)
