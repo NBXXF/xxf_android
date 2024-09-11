ad639e0d1f28c378d138df3f6003071d06ea7634
NBXXF <xuanyouwu@163.com>
2024-09-11 18:50:31 +0800

    feat:add ForegroundService api

--- lib_utils/src/main/java/com/xxf/utils/ProcessUtils.java
+++ lib_utils/src/main/java/com/xxf/utils/ProcessUtils.java
@@ -4,13 +4,16 @@ import android.app.ActivityManager;
 import android.app.Application;
 import android.content.Context;
 import android.os.Build;
+import android.os.Process;
 import android.text.TextUtils;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -143,5 +146,138 @@ public final class ProcessUtils {
 
     public static boolean isEmpty(Collection collection) {
         return collection == null || collection.isEmpty();
+    }
+
+    /**
+     * 获取正在运行的进程列表
+     *
+     * @return
+     */
+    public static List<ActivityManager.RunningAppProcessInfo> getRunningAppProcesses(Context context) {
+        try {
+            ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+            List<ActivityManager.RunningAppProcessInfo> runningAppProcesses = manager.getRunningAppProcesses();
+            if (runningAppProcesses == null) {
+                return Collections.emptyList();
+            }
+            return runningAppProcesses;
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        return Collections.emptyList();
+    }
+
+    /**
+     * 根据进程名字匹配 是否还活着的进程id
+     *
+     * @param kProcessName
+     * @return
+     */
+    public static List<ActivityManager.RunningAppProcessInfo> getProcess(Context context, String kProcessName) {
+        List<ActivityManager.RunningAppProcessInfo> filterProcess = new ArrayList<>();
+        List<ActivityManager.RunningAppProcessInfo> runningAppProcesses = getRunningAppProcesses(context);
+        for (ActivityManager.RunningAppProcessInfo runningAppProcess : runningAppProcesses) {
+            if (TextUtils.equals(runningAppProcess.processName, kProcessName)) {
+                filterProcess.add(runningAppProcess);
+            }
+        }
+        return filterProcess;
+    }
+
+    /**
+     * 进程是否活着
+     *
+     * @param kPid
+     * @return
+     */
+    public static boolean isLiving(Context context, int kPid) {
+        List<ActivityManager.RunningAppProcessInfo> runningAppProcesses = getRunningAppProcesses(context);
+        for (ActivityManager.RunningAppProcessInfo runningAppProcess : runningAppProcesses) {
+            if (runningAppProcess.pid == kPid) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 进程是否活着
+     *
+     * @param kProcessName
+     * @return
+     */
+    public static boolean isLiving(Context context, String kProcessName) {
+        List<ActivityManager.RunningAppProcessInfo> runningAppProcesses = getRunningAppProcesses(context);
+        for (ActivityManager.RunningAppProcessInfo runningAppProcess : runningAppProcesses) {
+            if (TextUtils.equals(kProcessName, runningAppProcess.processName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 杀掉app所有进程
+     *
+     * @param kPackageName
+     */
+    public static void waitKillAppComplete(Context context, String kPackageName) {
+        List<ActivityManager.RunningAppProcessInfo> runningAppProcesses = getRunningAppProcesses(context);
+        for (ActivityManager.RunningAppProcessInfo runningAppProcess : runningAppProcesses) {
+            String processName = runningAppProcess.processName;
+            if (TextUtils.equals(kPackageName, processName)
+                    || processName.startsWith(kPackageName)) {
+                waitKillProcessComplete(context,runningAppProcess.pid);
+            }
+        }
+    }
+
+    /**
+     * 等待彻底杀死
+     *
+     * @param kProcessName
+     */
+    public static void waitKillProcessComplete(Context context, String kProcessName) {
+        if (!TextUtils.isEmpty(kProcessName)) {
+            List<ActivityManager.RunningAppProcessInfo> processList = getProcess(context, kProcessName);
+            for (ActivityManager.RunningAppProcessInfo process : processList) {
+                if (process.pid >= 0) {
+                    waitKillProcessComplete(context, process.pid);
+                }
+            }
+        }
+    }
+
+
+    /**
+     * 等待彻底杀死
+     *
+     * @param kPid
+     */
+    public static void waitKillProcessComplete(Context context, int kPid) {
+        if (kPid >= 0) {
+            try {
+                int times = 0;
+                while (true) {
+                    times++;
+                    try {
+                        Process.killProcess(kPid);
+                    } catch (Throwable e) {
+                        e.printStackTrace();
+                    }
+                    if (times > 40 || !isLiving(context, kPid)) {
+                        break;
+                    }
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        times = Integer.MAX_VALUE;
+                    }
+                }
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
