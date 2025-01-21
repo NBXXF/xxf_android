6d7b5f0514c4127db238eb47f80d26831149cdb2
wuyouxuan <wuyouxuan@thinredline.com.cn>
2025-01-21 20:53:47 +0800

    feat:add api containsDetector getValueNullable

--- optional/lib_mlkit/src/main/java/com/xxf/mlkit/Result.kt
+++ optional/lib_mlkit/src/main/java/com/xxf/mlkit/Result.kt
@@ -8,3 +8,21 @@ import com.google.mlkit.vision.interfaces.Detector
 fun <T> com.xxf.mlkit.MlKitFastAnalyzer.Result.containsDetector(detector: Detector<T>?): Boolean {
     return mValues.contains(detector) || mThrowables.contains(detector);
 }
+
+/**
+ * 获取识别结果
+ * 相比于[com.xxf.mlkit.MlKitFastAnalyzer.Result.getValue] 没有检查
+ */
+@Suppress("UNCHECKED_CAST")
+fun <T> com.xxf.mlkit.MlKitFastAnalyzer.Result.getValueNullable(detector: Detector<T>?): T? {
+    return mValues[detector] as T?
+}
+
+/**
+ * 获取异常结果
+ * 相比于[com.xxf.mlkit.MlKitFastAnalyzer.Result.getThrowable] 没有检查
+ */
+@Suppress("UNCHECKED_CAST")
+fun <T> com.xxf.mlkit.MlKitFastAnalyzer.Result.getThrowableNullable(detector: Detector<T>?): Throwable? {
+    return mThrowables[detector]
+}
